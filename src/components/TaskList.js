24b0406c9d4b42f589f4149242c545f33162ce73
TumiMoshou <139065767+TumiMoshou@users.noreply.github.com>
2024-03-24 10:53:06 +0200

    Add functionality to delete tasks from the list

---
+++ src/components/TaskList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TaskList({ tasks, deleteTask }) {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>
+          <span>{task.name}</span>
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TaskList;
